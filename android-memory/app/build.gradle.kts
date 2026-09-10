dc34f1a9c8ef9f203291272c840e95ecf8a76ed0
xuchangzhen <3237396275@qq.com>
2026-09-10 09:40:15 +0800

    release: v0.9.7

--- android-memory/app/build.gradle.kts
+++ android-memory/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.linguabridge.memory"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.4.6"
+        versionCode = 13
+        versionName = "0.4.8"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
